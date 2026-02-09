14331619d2d45cebc8698d0633c4985a63819be9
Daulton <jenkins.daulton@gmail.com>
2026-02-09 03:47:15 -0400

    roguelite pwa v0.1

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Roguelike PWA',
+        short_name: 'Roguelike',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#070a0e',
+        theme_color: '#070a0e',
+        icons: [
+          { src: '/pwa-192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/pwa-512.png', sizes: '512x512', type: 'image/png' }
+        ]
+      }
+    })
+  ]
+})
